5d346f3edd45d2733a57ce1c3f00de4eb64a27f8
Onii-Chan <140295418+waqarbaig6133@users.noreply.github.com>
2024-04-09 22:39:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ from nltk.corpus import words
 words = words.words()
 letters = input("What are the letters present: ")
 
+available_words = []
+
 for x in english_words:
   intersection = list(set(letters) & set(english_words))
   if len(intersection) == len(list(x)):
-    print(x)
+    available_words.append(x)
+print(sorted(avaiable_words, key = len))
     
 
